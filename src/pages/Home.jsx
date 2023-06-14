e6999d7bacdb1b7704428ec83ebc1a2be3df6603
sabeern <sabeerneyyan@gmail.com>
2023-06-14 19:22:10 +0530

    Created logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,26 @@
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="bg-[#2874F0]">
+        <nav className="px-44 py-2">
+          <img
+            src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/flipkart-plus_8d85f4.png"
+            alt="logo"
+            className="w-18 h-4"
+          />
+          <p className="inline">
+            <span className="text-white text-xs">Expolre</span>{" "}
+            <span className="text-[#daca3a] text-sm">Plus</span>
+          </p>
+          <img
+            src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/plus_aef861.png"
+            alt="icon"
+            className="inline w-5 h-5"
+          />
+        </nav>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
