e7aa2365aa8738ba803c6c6c750ce4897c85f228
sabeern <sabeerneyyan@gmail.com>
2023-06-21 23:10:28 +0530

    Added icon for search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,12 +19,21 @@ function Home() {
               className="inline w-5 h-5"
             />
           </div>
-          <div className="ml-4">
+          <div className="flex items-center ml-4">
             <input
               type="text"
               className="text-sm w-80 h-8 px-4"
               placeholder="Search for products, brands and more"
             />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="w-6 h-6 ml-[-30px] text-blue-700"
+            >
+              <path d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+            </svg>
           </div>
         </nav>
       </div>
